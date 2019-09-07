83c8c3bf5f5d59f09a598eabb45183fac0cadf4b
HyeonCheol Cho <crocuis@gmail.com>
2019-09-07 16:20:03 +0900

    fix for unreal.js

--- src/api/api.cc
+++ src/api/api.cc
@@ -1329,7 +1329,7 @@ void Template::Set(v8::Local<Name> name, v8::Local<Data> value,
   ENTER_V8_NO_SCRIPT_NO_EXCEPTION(isolate);
   i::HandleScope scope(isolate);
   auto value_obj = Utils::OpenHandle(*value);
-  CHECK(!value_obj->IsJSReceiver() || value_obj->IsTemplateInfo());
+  // CHECK(!value_obj->IsJSReceiver() || value_obj->IsTemplateInfo());
   if (value_obj->IsObjectTemplateInfo()) {
     templ->set_serial_number(i::Smi::kZero);
     if (templ->IsFunctionTemplateInfo()) {
